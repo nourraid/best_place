74e89f11e7af2ad05733704923fc16978dd94f34
nrayd <nrayd633@gmail.com>
2023-06-14 13:56:11 +0300

    start frontsite

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -15,6 +15,8 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 </head>
+
+
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
